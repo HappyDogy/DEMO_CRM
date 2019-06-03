b401eabdc2b6251b49041656f0f251df75544df5
Happy <2572678724@qq.com>
2019-06-03 21:37:41 +0800

    Signed-off-by: Happy <2572678724@qq.com>

---
+++ files/移入公海/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,H,I,H,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[])),X,_(),Y,_());}; 
+var b="url",c="移入公海.html",d="generationDate",e=new Date(1559568917944.97),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="f20948d1b74549d191f7e2a4d709cf32",q="type",r="Axure:Page",s="name",t="移入公海",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="center",A="fill",B="fillType",C="solid",D="color",E=0xFFF5F5F5,F="image",G="imageHorizontalAlignment",H="near",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
+return _creator();
+})());
